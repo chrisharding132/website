f743755c78fe0b2c448756c68c7089e651ecbcf4
Chris Harding <charding132@gmail.com>
2013-10-30 19:31:44 -0700

    Finished basic website

--- app.py
+++ app.py
@@ -8,10 +8,18 @@ app.config.update(
 )
 
 # controllers
-@app.route("/")
-def hello():
-    return "Hello from Python!"
+@app.route("/favicon.ico")
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'), 'ico/favicon.ico')
     
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+    
+@app.route("/")
+def index():
+    return render_template('index.html')
+
 #launch
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
